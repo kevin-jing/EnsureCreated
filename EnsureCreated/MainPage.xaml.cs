c64d1a3729b2cfb7d2e341012ea2d9786f6e2c92
kevin-jing <jingqf@hotmail.com>
2018-07-09 23:17:24 +0800

    Initial commit

---
+++ EnsureCreated/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EnsureCreated
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (var db = new BloggingContext())
+            {
+                Blogs.ItemsSource = db.Blogs.ToList();
+            }
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new BloggingContext())
+            {
+                var blog = new Blog { Url = NewBlogUrl.Text };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                Blogs.ItemsSource = db.Blogs.ToList();
+            }
+        }
+    }
+}
